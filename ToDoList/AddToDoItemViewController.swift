1c037ce7b57e3ee6a02c4ff155fd163a034d3430
Kevin Xu <kevin@imkevinxu.com>
2014-11-25 06:21:57 -0800

    Custom UIViewController and TableViewController classes

---
+++ ToDoList/AddToDoItemViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddToDoItemViewController.swift
+//  ToDoList
+//
+//  Created by Kevin Xu on 11/25/14.
+//  Copyright (c) 2014 Kevin Xu. All rights reserved.
+//
+
+import UIKit
+
+class AddToDoItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
